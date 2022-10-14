d689c9e29635fe02421e79dd06f3e57a2725b3d6
hemdee <sanusi5110@gmail.com>
2022-10-14 06:51:40 +0100

    fixed issues

--- src/components/collectionArticle.jsx
+++ src/components/collectionArticle.jsx
@@ -8,7 +8,7 @@ const CollectionArticle = ({ image, title, artist }) => {
         alt="album cover"
         className="w-[100%] h-[45vh] sm:h-60 sm:w-56 object-cover"
       />
-      <div className="absolute bottom-0 left-0 right-0 pl-4 pb-4 translate-y-[40%] transition-all cont">
+      <div className="absolute bottom-0 left-0 right-0 pl-4 pb-4 sm:translate-y-[40%] translate-y-0 transition-all cont">
         <h2 className="text-xl font-bold">{title}</h2>
         <h3 className="text-sm text-gray-300">{artist}</h3>
         <h3 className="mt-8">2.3m likes</h3>
@@ -16,7 +16,7 @@ const CollectionArticle = ({ image, title, artist }) => {
           -
         </div>
       </div>
-      <div className="ml-4 sm:ml-10 cursor-pointer p-2 rounded-full bg-primary-yellow w-10 absolute right-4 bottom-4 opacity-0 icon">
+      <div className="ml-4 sm:ml-10 cursor-pointer p-2 rounded-full bg-primary-yellow w-10 absolute right-4 bottom-4 sm:opacity-0 opacity-100 icon">
         <Play variant="Bold" size="24" />
       </div>
     </article>
